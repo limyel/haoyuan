f27952dbfc6395a9748a641811ae9d461292aa79
limyel <limyel@outlook.com>
2022-04-19 20:21:05 +0800

    init

---
+++ src/main/java/com/example/haoyuanmpbackend/HaoyuanMpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.haoyuanmpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyuanMpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyuanMpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/haoyuanmpbackend/HaoyuanMpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haoyuanmpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoyuanMpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
