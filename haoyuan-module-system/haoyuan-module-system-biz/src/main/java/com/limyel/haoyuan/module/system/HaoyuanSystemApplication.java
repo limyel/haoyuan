9ef42208cdaf97623f8e4d95c2f2849cb52889d6
limyel <limyel@outlook.com>
2024-01-10 19:31:46 +0800

    新增模块

---
+++ haoyuan-module-system/haoyuan-module-system-biz/src/main/java/com/limyel/haoyuan/module/system/HaoyuanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.limyel.haoyuan.module.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyuanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyuanSystemApplication.class, args);
+    }
+
+}
