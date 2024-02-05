fa47ccb972ea62f8ffb19e719a9e9e946d73fcc6
limyel <limyel@outlook.com>
2024-02-05 18:57:43 +0800

    fix

--- haoyuan-module-system/haoyuan-module-system-biz/src/main/java/com/limyel/haoyuan/module/system/auth/service/impl/UserDetailsServiceImpl.java
+++ haoyuan-module-system/haoyuan-module-system-biz/src/main/java/com/limyel/haoyuan/module/system/auth/service/impl/UserDetailsServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl impl ements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private SysUserService sysUserService;
