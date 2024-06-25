81a4b719a3faab6e85fe7500635624568814d750
limyel <limyel@outlook.com>
2024-06-25 17:29:06 +0800

    feat: 用户初始化

---
+++ haoyuan-blog/blog-startup/src/main/resources/db/migration/V1_1__Init_User.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admin_user` (`id`, `username`, `password`) values (1, 'limyel', '$2a$10$ahpMO/WKLzrrNbRzRkCm5Om23ytX9xklLTfs3wG.dsgdGpj.7s2ne');
+
+INSERT INTO `admin_user_role` (`user_id`, `role`) values (1, 'ROLE_ADMIN');
