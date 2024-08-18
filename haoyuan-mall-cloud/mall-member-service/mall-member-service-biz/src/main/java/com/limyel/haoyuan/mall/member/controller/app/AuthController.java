aae3841dfd667ae3df74f13119bb781e5985c673
limyel <limyel@outlook.com>
2024-08-19 00:17:05 +0800

    test: 测试部署

--- haoyuan-mall-cloud/mall-member-service/mall-member-service-biz/src/main/java/com/limyel/haoyuan/mall/member/controller/app/AuthController.java
+++ haoyuan-mall-cloud/mall-member-service/mall-member-service-biz/src/main/java/com/limyel/haoyuan/mall/member/controller/app/AuthController.java
@@ -2,7 +2,7 @@ package com.limyel.haoyuan.mall.member.controller.app;
 
 import com.limyel.haoyuan.common.core.pojo.R;
 import com.limyel.haoyuan.mall.member.dto.auth.LoginDTO;
-import com.limyel.haoyuan.mall.member.service.UserService;
+import com.limyel.haoyuan.mall.member.service.AuthService;
 import com.limyel.haoyuan.mall.member.vo.auth.LoginVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public R<LoginVO> login(@Validated @RequestBody LoginDTO dto) {
-        LoginVO result = userService.login(dto);
+        LoginVO result = authService.login(dto);
         return R.ok(result);
     }
 
