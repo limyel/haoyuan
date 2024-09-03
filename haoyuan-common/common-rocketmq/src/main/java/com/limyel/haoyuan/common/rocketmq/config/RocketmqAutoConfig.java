fd0daa760b41ddc4df54fdced02c0dcf4111caef
limyel <limyel@outlook.com>
2024-09-03 21:27:47 +0800

    refector: 重构

---
+++ haoyuan-common/common-rocketmq/src/main/java/com/limyel/haoyuan/common/rocketmq/config/RocketmqAutoConfig.java
@@ -0,0 +1,10 @@
+package com.limyel.haoyuan.common.rocketmq.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+@AutoConfiguration
+public class RocketmqAutoConfig {
+
+
+
+}
