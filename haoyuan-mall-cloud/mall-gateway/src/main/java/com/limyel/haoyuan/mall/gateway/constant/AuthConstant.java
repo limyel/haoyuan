cd4cac729afdf96f371cd7e72f907f5366006536
limyel <limyel@outlook.com>
2024-09-10 14:02:50 +0800

    refector: 重构 mall-gateway

--- haoyuan-mall-cloud/mall-gateway/src/main/java/com/limyel/haoyuan/mall/gateway/constant/AuthConstant.java
+++
@@ -1,9 +0,0 @@
-package com.limyel.haoyuan.mall.gateway.constant;
-
-public interface AuthConstant {
-
-    String AUTHORITY_PREFIX = "ROLE_";
-
-    String AUTHORITY_CLAIM_NAME = "authorities";
-
-}
