f974cce504afd9969aeb7098c363b297a2be69f5
limyel <limyel@outlook.com>
2024-09-10 17:16:54 +0800

    feat: 补充认证授权相关类

---
+++ haoyuan-mall-cloud/mall-sys-service/mall-sys-service-biz/src/main/java/com/limyel/haoyuan/mall/sys/dao/MenuDao.java
@@ -0,0 +1,10 @@
+package com.limyel.haoyuan.mall.sys.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.limyel.haoyuan.mall.sys.entity.MenuEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MenuDao extends BaseMapper<MenuEntity> {
+
+}
---
+++ haoyuan-mall-cloud/mall-sys-service/mall-sys-service-biz/src/main/java/com/limyel/haoyuan/mall/sys/service/RoleMenuService.java
@@ -0,0 +1,29 @@
+package com.limyel.haoyuan.mall.sys.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.limyel.haoyuan.mall.sys.dao.RoleMenuDao;
+import com.limyel.haoyuan.mall.sys.entity.RoleMenuEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoleMenuService {
+
+    private final RoleMenuDao roleMenuDao;
+
+    List<Long> getMenuIdList(List<Long> roleIds) {
+        List<RoleMenuEntity> list = roleMenuDao.selectList(new LambdaQueryWrapper<RoleMenuEntity>()
+                .in(RoleMenuEntity::getRoleId, roleIds));
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return list.stream()
+                .map(RoleMenuEntity::getMenuId)
+                .toList();
+    }
+
+}
