9a418ea3bd7940e1270b93cfa73d93cf277c36d0
limyel <limyel@outlook.com>
2024-10-15 13:50:44 +0800

    feat: 角色相关接口文档补充

--- haoyuan-cloud/haoyuan-sys-service/haoyuan-sys-service-biz/src/main/java/com/limyel/haoyuan/cloud/sys/controller/admin/RoleController.java
+++ haoyuan-cloud/haoyuan-sys-service/haoyuan-sys-service-biz/src/main/java/com/limyel/haoyuan/cloud/sys/controller/admin/RoleController.java
@@ -8,6 +8,8 @@ import com.limyel.haoyuan.common.core.pojo.R;
 import com.limyel.haoyuan.common.core.validator.group.Create;
 import com.limyel.haoyuan.common.core.validator.group.Update;
 import com.limyel.haoyuan.common.mybatis.pojo.PageData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,34 +25,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/role")
 @RequiredArgsConstructor
+@Tag(name = "角色管理")
 public class RoleController {
 
     private final RoleService roleService;
 
     @PostMapping("/create")
+    @Operation(summary = "创建角色")
     public R<?> create(@Validated(Create.class) @RequestBody RoleDTO dto) {
         // todo 校验？
         return R.check(roleService.create(dto));
     }
 
     @GetMapping("/delete/{ids}")
+    @Operation(summary = "删除角色")
     public R<?> deleteByIds(@PathVariable("ids") Long[] ids) {
         List<Long> list = Arrays.asList(ids);
         return R.check(roleService.deleteByIds(list));
     }
 
     @PostMapping("/update")
+    @Operation(summary = "更新角色")
     public R<?> update(@Validated(Update.class) @RequestBody RoleDTO dto) {
         return R.check(roleService.update(dto));
     }
 
     @GetMapping("/get-page")
+    @Operation(summary = "角色分页")
     public R<PageData<RolePageVO>> getPage(@Validated RolePageDTO dto) {
         PageData<RolePageVO> result = roleService.getPage(dto);
         return R.ok(result);
     }
 
     @GetMapping("/get/{id}")
+    @Operation(summary = "角色详情")
     public R<?> getById(@PathVariable("id") Long id) {
         RoleDTO result = roleService.getById(id);
         return R.ok(result);
--- haoyuan-cloud/haoyuan-sys-service/haoyuan-sys-service-biz/src/main/java/com/limyel/haoyuan/cloud/sys/dao/DeptDao.java
+++ haoyuan-cloud/haoyuan-sys-service/haoyuan-sys-service-biz/src/main/java/com/limyel/haoyuan/cloud/sys/dao/DeptDao.java
@@ -1,4 +1,7 @@
 package com.limyel.haoyuan.cloud.sys.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface DeptDao {
 }
