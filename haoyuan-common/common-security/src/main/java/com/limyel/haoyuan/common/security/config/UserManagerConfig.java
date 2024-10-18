43240977d3880ea0c5dcec95fc142fd9b9bdffcd
limyel <limyel@outlook.com>
2024-10-18 15:56:59 +0800

    refector: 重构

--- haoyuan-common/common-security/src/main/java/com/limyel/haoyuan/common/security/config/UserManagerConfig.java
+++ haoyuan-common/common-security/src/main/java/com/limyel/haoyuan/common/security/config/UserManagerConfig.java
@@ -3,10 +3,11 @@ package com.limyel.haoyuan.common.security.config;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @AutoConfiguration
-public class PasswordEncoderConfig {
+public class UserManagerConfig {
 
     /**
      * 密码加密
@@ -14,6 +15,7 @@ public class PasswordEncoderConfig {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
 }
